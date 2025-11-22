5d85cd9c3914bd3f28a593dfec0ac942b9f71bc7
Kate Tiffany Bautista <katetiffanyquintobautista@gmail.com>
2025-11-22 20:57:09 +0800

    Move requirements.txt to root

--- school_map_project/schoolmap/schoolmap/settings.py
+++ school_map_project/schoolmap/schoolmap/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)m(bv)qzo$*=^^72^oej9!vgidkphx2ub8gmp+wqydp=uad%b-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+import os
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']  # Update this with your domain
 
 
 # Authentication settings
@@ -123,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files (User uploaded files)
 MEDIA_URL = '/media/'
