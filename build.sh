e1d0e61268227004270c4b4a23a9ddb83b26b6e1
Kate Tiffany Bautista <katetiffanyquintobautista@gmail.com>
2025-11-23 13:39:28 +0800

    Simplify deployment - remove heavy dependencies

--- build.sh
+++ build.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
-# Use pip cache for faster builds
-pip install --upgrade pip
-pip install -r requirements.txt --cache-dir /opt/render/project/.pip-cache
+pip install -r requirements.txt
 
 cd school_map_project/schoolmap
-python manage.py collectstatic --no-input
-python manage.py migrate
+python manage.py collectstatic --no-input --clear
