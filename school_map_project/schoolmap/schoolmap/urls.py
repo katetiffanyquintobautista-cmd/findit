4c4feb310b446c61f4ed8c0d38000b6e227bebb7
Kate Tiffany Bautista <katetiffanyquintobautista@gmail.com>
2025-11-27 04:08:44 +0800

    Fix root URL to redirect to landing page

--- school_map_project/schoolmap/schoolmap/urls.py
+++ school_map_project/schoolmap/schoolmap/urls.py
@@ -28,6 +28,8 @@ urlpatterns = [
     # Include Django's auth URLs but point them to our custom login view
     path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(next_page='landing'), name='logout'),
+    # Direct root path to landing page
+    path('', RedirectView.as_view(url='/landing/', permanent=False), name='root_redirect'),
     path('', include('mapapp.urls')),
 ]
 
